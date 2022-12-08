84670a9bd66d38f50d3c41dac5c0313771f94730
Emmanuel <obikennethe913@gmail.com>
2022-12-08 18:11:44 +0200

    initial commit

--- js/app.js
+++ js/app.js
@@ -7,7 +7,8 @@ import * as THREE from "./mods/three.module.js"
 import Stats from "./mods/stats.module.js"
 import { OrbitControls } from "./mods/OrbitControls.js"
 
-import { Water } from "./mods/Water2.js" // lib for water effect
+import { TWEEN } from "./mods/tween.module.min.js" // module for tween animations
+import { GUI } from "./mods/lil-gui.module.min.js"
 
 // Global variables
 let mainContainer = null
@@ -16,31 +17,40 @@ let stats = null
 let camera = null
 let renderer = null
 let scene = null
+let fishes = null
 
 let camControls = null
 // Global Meshes
 
-let plane,
-  floor,
-  box,
-  sphere,
-  cone = null
-
-let water = null
-
+let fish1,
+  fish2,
+  bubbles,
+  gui,
+  starFish,
+  water = null
+let controlBoxParams = {
+  rotateSpeed: 0,
+  y: 0,
+  x: 0,
+  z: 0,
+}
 function init() {
   fpsContainer = document.querySelector("#fps")
   mainContainer = document.querySelector("#webgl-scene")
   scene = new THREE.Scene()
+  const cubeTextureLoader = new THREE.CubeTextureLoader()
+  cubeTextureLoader.setPath("img/water/")
 
-  const background2 = new THREE.TextureLoader().load("img/desert.jpg")
-  let skyGeometry = new THREE.SphereGeometry(90, 32, 32)
-  let skyMaterial = new THREE.MeshBasicMaterial({
-    map: background2,
-    side: THREE.BackSide,
-  })
-  let skyBox = new THREE.Mesh(skyGeometry, skyMaterial)
-  scene.add(skyBox)
+  const cubeTexture = cubeTextureLoader.load([
+    "px.png",
+    "nx.png",
+    "py.png",
+    "ny.png",
+    "pz.png",
+    "nz.png",
+  ])
+
+  scene.background = cubeTexture
 
   createStats()
   createCamera()
@@ -48,6 +58,7 @@ function init() {
   createMeshes()
   createLights()
   createRenderer()
+  createCtrBox()
   renderer.setAnimationLoop(() => {
     update()
     render()
@@ -55,7 +66,9 @@ function init() {
 }
 
 // Animations
-function update() {}
+function update() {
+  TWEEN.update()
+}
 
 // Statically rendered content
 function render() {
@@ -73,9 +86,9 @@ function createStats() {
 
 // Camera object
 function createCamera() {
-  const fov = 45
+  const fov = 60
   const aspect = mainContainer.clientWidth / mainContainer.clientHeight
-  const near = 0.1
+  const near = 1
   const far = 500 // meters
   camera = new THREE.PerspectiveCamera(fov, aspect, near, far)
   camera.position.x = 10
@@ -89,349 +102,137 @@ function createControls() {
   camControls = new OrbitControls(camera, mainContainer)
   camControls.autoRotate = false
 }
-
+function createCtrBox() {
+  gui = new GUI()
+  gui
+    .add(controlBoxParams, "z", -60, 30)
+    .min(1000)
+    .max(7000)
+    .step(50)
+    .onChange(function (value) {
+      const tween = new TWEEN.Tween(fishes.rotation)
+        .to({ y: Math.PI / 2 }, value) // relative animation
+        .onComplete(function () {
+          if (Math.abs(fishes.rotation.y) >= 2 * Math.PI) {
+            fishes.rotation.y = fishes.rotation.y % (2 * Math.PI)
+          }
+        })
+        .start()
+      tween.repeat(Infinity)
+    })
+    .name("rotation")
+  gui
+    .add(controlBoxParams, "x", -30, 30)
+    .step(5)
+    .onChange(function (value) {
+      fish2.position.x = value
+    })
+    .name("Fish 2 ")
+}
 // Light objects
 function createLights() {
-  const spotLight = new THREE.SpotLight(0xffffff)
-  spotLight.position.set(-10, 18, 10)
-  spotLight.shadow.mapSize.width = 2048 // default 512
-  spotLight.shadow.mapSize.height = 2048 //default 512
-  spotLight.intensity = 1.5
-  spotLight.distance = 200
-  spotLight.angle = Math.PI / 3
-  spotLight.penumbra = 0.4 // 0 - 1
-  spotLight.decay = 0.2
-  spotLight.castShadow = true
-  //spotLight.target = sphere;
-  scene.add(spotLight)
-
-  const ambientLight = new THREE.AmbientLight(0xffffff, 0.2) // 0x111111 - 0xaaaaaa, 1 ; 0xffffff, 0.1 - 0.3; 0x404040
+  const ambientLight = new THREE.AmbientLight(0xffffff, 1)
   scene.add(ambientLight)
 }
 
-function createPlane() {
-  const texture = new THREE.TextureLoader().load("img/Rock_texture.jpg") // load texture
-  texture.anisotropy = 16 // set anisotropy coef.
-
-  // Set min max texture filters
-  // texture.magFilter = THREE.NearestFilter;
-  texture.magFilter = THREE.LinearFilter // default mag filter
-
-  texture.minFilter = THREE.LinearMipMapLinearFilter // default min filter
-
-  // set repeating params
-  texture.wrapS = THREE.RepeatWrapping
-  texture.wrapT = THREE.RepeatWrapping
-  texture.repeat.set(3, 3)
-  // texture.repeat.set(0.5, 0.5); // zoom
-  // texture.repeat.set(-2, -2); // repeat reversed
-
-  const circleGeometry = new THREE.CircleGeometry(30, 30)
-  // const planeMaterial =  new THREE.MeshLambertMaterial({color: 0xcccccc});
-  const planeMaterial = new THREE.MeshStandardMaterial({ map: texture }) // map texture to mesh
-  plane = new THREE.Mesh(circleGeometry, planeMaterial)
-  plane.rotation.x = -0.5 * Math.PI
-  plane.position.x = 0
-  plane.position.y = 0
-  plane.position.z = 0
-
-  plane.receiveShadow = true
-  scene.add(plane)
-}
-function createFloor() {
-  const geometry = new THREE.PlaneGeometry(16, 16)
-  const texture = new THREE.TextureLoader().load("img/wood.jpg")
-  texture.wrapS = THREE.RepeatWrapping
-  texture.wrapT = THREE.RepeatWrapping
-  texture.repeat.set(2, 2)
-
-  const material = new THREE.MeshStandardMaterial({ map: texture })
-  const floor = new THREE.Mesh(geometry, material)
-
-  floor.rotation.x = -0.5 * Math.PI
-  floor.position.x = -10
-  floor.position.z = -10
-  floor.position.y = 1
-
-  scene.add(floor)
-}
-function createSideWall() {
-  const shape = new THREE.Shape()
-  shape.moveTo(-8, 0)
-  shape.lineTo(8, 0)
-  shape.lineTo(8, 8)
-  shape.lineTo(0, 18)
-  shape.lineTo(-8, 8)
-  shape.lineTo(-8, 0)
-  const extrudeGeometry = new THREE.ExtrudeGeometry(shape)
-  const texture = new THREE.TextureLoader().load("img/wall_bricks_images.jpg")
-  // texture.repeat.set(0.5, 0.5)
-  const bump = new THREE.TextureLoader().load("img/brick_bump.jpg") // load bump map
-  bump.wrapS = THREE.RepeatWrapping
-  bump.wrapT = THREE.RepeatWrapping
-  bump.repeat.set(0.5, 0.5)
-
-  // Load normal map
-  let normal = new THREE.TextureLoader().load("img/wall_brick_normal.jpg") // load bump map
-  normal.wrapS = THREE.RepeatWrapping
-  normal.wrapT = THREE.RepeatWrapping
-  normal.repeat.set(0.5, 0.5)
-
-  texture.wrapS = texture.wrapT = THREE.RepeatWrapping
-  texture.repeat.set(0.1, 0.05)
-  const material = new THREE.MeshStandardMaterial({ map: texture })
-  const sideWall = new THREE.Mesh(extrudeGeometry, material)
-  sideWall.material.map.wrapS = THREE.RepeatWrapping
-  sideWall.material.map.wrapT = THREE.RepeatWrapping
-  sideWall.material.map.repeat.set(0.5, 0.5) // zoom texture
-
-  material.bumpMap = bump // add map to material
-  material.bumpScale = 0.3 // you can scale bump in or out [-1;1]
-
-  material.normalMap = normal // add map to material
-  material.normalScale = new THREE.Vector2(0.5, 0, 5) // you can scale normal in or out [0;1]
-
-  sideWall.position.x = -10
-  sideWall.position.z = -18
-  sideWall.position.y = 1
-  sideWall.receiveShadow = true
-  sideWall.castShadow = true
-  scene.add(sideWall)
-  return sideWall
-}
-function createBackWall() {
-  const shape = new THREE.Shape()
-  shape.moveTo(-7.5, 0)
-  shape.lineTo(8.5, 0)
-  shape.lineTo(8.5, 8.5)
-  shape.lineTo(-7.5, 8.5)
-  const extrudeGeometry = new THREE.ExtrudeGeometry(shape)
-  const texture = new THREE.TextureLoader().load("img/wall_bricks_images.jpg")
-  texture.wrapS = texture.wrapT = THREE.RepeatWrapping
-  texture.repeat.set(0.1, 0.05)
-  const material = new THREE.MeshStandardMaterial({ map: texture })
-  const backWall = new THREE.Mesh(extrudeGeometry, material)
-  backWall.position.z = -9.5
-  backWall.position.x = -18
-  backWall.position.y = 1
-  backWall.rotation.y = Math.PI * 0.5
-  scene.add(backWall)
-}
-function createFrontWall() {
-  const shape = new THREE.Shape()
-  shape.moveTo(-8, 0)
-  shape.lineTo(8, 0)
-  shape.lineTo(8, 8)
-  shape.lineTo(-8, 8)
-  shape.lineTo(-8, 0)
-  const window = new THREE.Path()
-  window.moveTo(3, 3)
-  window.lineTo(8, 3)
-  window.lineTo(8, 8)
-  window.lineTo(3, 8)
-  window.lineTo(3, 3)
-  shape.holes.push(window)
-  const door = new THREE.Path()
-  door.moveTo(-3, 0)
-  door.lineTo(-3, 8)
-  door.lineTo(-8, 8)
-  door.lineTo(-8, 0)
-  door.lineTo(-3, 0)
-  shape.holes.push(door)
-  const extrudeGeometry = new THREE.ExtrudeGeometry(shape)
-  const texture = new THREE.TextureLoader().load("img/wall_bricks_images.jpg")
-  texture.wrapS = texture.wrapT = THREE.RepeatWrapping
-  texture.repeat.set(0.1, 0.05)
-  const material = new THREE.MeshStandardMaterial({ map: texture })
-  const frontWall = new THREE.Mesh(extrudeGeometry, material)
-  frontWall.position.z = -10
-  frontWall.position.x = -3
-  frontWall.position.y = 1
-  frontWall.rotation.y = Math.PI * 0.5
-  scene.add(frontWall)
-}
-
-function createRoof() {
-  const geometry = new THREE.BoxGeometry(14, 17, 1)
-  // const texture = new THREE.TextureLoader().load("img/tile.jpg")
-  // texture.wrapS = texture.wrapT = THREE.RepeatWrapping
-  // texture.repeat.set(2, 1)
-  // texture.rotation = Math.PI / 2
-  // const textureMaterial = new THREE.MeshStandardMaterial({ map: texture })
-  const colorMaterial = new THREE.MeshStandardMaterial({ color: "black" })
-  // const materials = [
-  //   colorMaterial,
-  //   colorMaterial,
-  //   colorMaterial,
-  //   colorMaterial,
-  //   colorMaterial,
-  //   textureMaterial,
-  // ]
-  const roof = new THREE.Mesh(geometry, colorMaterial)
-
-  scene.add(roof)
-  roof.rotation.x = Math.PI / 2
-  roof.rotation.y = (-Math.PI / 4) * 1.15
-  roof.position.x = -5
-  roof.position.y = 14
-  roof.position.z = -10
-  return roof
-}
-function createGarageRoof() {
-  const geometry = new THREE.BoxGeometry(21, 21, 1)
-  const texture = new THREE.TextureLoader().load("img/favour.jpg")
-  texture.anisotropy = 16
-
-  const bump = new THREE.TextureLoader().load("img/name_bump.jpg") // load bump map
-  // const bump = new THREE.TextureLoader().load("img/wood_bump.jpg")
-  texture.wrapS = texture.wrapT = THREE.RepeatWrapping
-  texture.repeat.set(1, 1)
-  texture.rotation = Math.PI / 2
-  const textureMaterial = new THREE.MeshStandardMaterial({
-    map: texture,
-    bumpMap: bump,
-    bumpScale: 0.5,
-  })
-  bump.wrapS = THREE.RepeatWrapping
-  bump.wrapT = THREE.RepeatWrapping
-  bump.repeat.set(1, 1)
-  const roof = new THREE.Mesh(geometry, textureMaterial)
-
-  scene.add(roof)
-  roof.rotation.x = Math.PI / 2
-  roof.position.x = -3
-  roof.position.z = 15
-  roof.position.y = 15
-  return roof
-}
-function createDoor() {
-  let door = new THREE.PlaneGeometry(4, 7.5, 15)
-  const texture = new THREE.TextureLoader().load("img/wooden_door.jpg")
-
-  // texture.anisotropy = 30
-  texture.wrapS = THREE.RepeatWrapping
-  texture.wrapT = THREE.RepeatWrapping
-  texture.repeat.set(1, 1)
-  let doorMaterial = new THREE.MeshStandardMaterial({ map: texture })
-  let doorMesh = new THREE.Mesh(door, doorMaterial)
-  doorMesh.rotation.y = Math.PI * 0.5
-  doorMesh.position.x = -1.85
-  doorMesh.position.y = 5
-  doorMesh.position.z = -5
-  scene.add(doorMesh)
-}
-
-function createWindow() {
-  let window = new THREE.PlaneGeometry(4, 6, 7.5)
-  const texture = new THREE.TextureLoader().load("img/wooden_door.jpg")
-
-  // texture.anisotropy = 30
-  texture.wrapS = THREE.RepeatWrapping
-  texture.wrapT = THREE.RepeatWrapping
-  texture.repeat.set(1, 1)
-  let windowMaterial = new THREE.MeshStandardMaterial({ map: texture })
-  let windowMesh = new THREE.Mesh(window, windowMaterial)
-  windowMesh.rotation.y = Math.PI * 0.5
-  windowMesh.position.x = -1.85
-  windowMesh.position.y = 7
-  windowMesh.position.z = -15
-  scene.add(windowMesh)
-}
-
-function createWater() {
-  let waterParams = {
-    color: "#93B0FF",
-    scale: 4,
-    flowX: 0.6,
-    flowY: 0.6,
-    // sound: true
-  }
-  const waterGeometry = new THREE.CircleGeometry(30, 30)
-  water = new Water(waterGeometry, {
-    color: waterParams.color,
-    scale: waterParams.scale,
-    flowDirection: new THREE.Vector2(waterParams.flowX, waterParams.flowY),
-    textureWidth: 1024,
-    textureHeight: 1024,
-  })
-  water.rotation.x = -0.5 * Math.PI
-  water.position.y = 0.5
-  water.position.x = 0
-  water.position.z = 0
-  // water.position.y = 1
-  // water.position.x = 10
-  // water.position.z = 10
-  scene.add(water)
-}
-function createRod() {
-  const geometry = new THREE.CylinderGeometry(
-    0.4,
-    0.4,
-    15,
-    35,
-    8,
-    false,
-    0,
-    6.28
-  )
-  const material = new THREE.MeshStandardMaterial({ color: 0x540c0f })
-  const cylinder = new THREE.Mesh(geometry, material)
-  scene.add(cylinder)
-  cylinder.position.set(-10, 7.5, 23)
-  cylinder.receiveShadow = true
-  cylinder.castShadow = true
-
-  // cylinder.position.x = -9.5
-  // cylinder.position.y = 2.5
-  // cylinder.position.z = 3.1
-
-  scene.add(cylinder)
-  return cylinder
-}
-
 // Create sprite
-function createCloud() {
-  const cloudTexture = new THREE.TextureLoader().load(
-    "img/cloud-removebg-preview.png"
-  )
-  const cloudMaterial = new THREE.SpriteMaterial({
-    map: cloudTexture,
+function createBubbles() {
+  const bubblesTexture = new THREE.TextureLoader().load("img/bubbles.png")
+  const bubblesMaterial = new THREE.SpriteMaterial({
+    map: bubblesTexture,
     color: 0xffffff,
   })
-  // const cloudMaterial = new THREE.SpriteMaterial( { map: cloudTexture, color: 0xffffff, transparent:true, opacity:0.7 } );
-  const cloud = new THREE.Sprite(cloudMaterial)
-  cloud.scale.set(20, 10, 1)
-  cloud.position.set(5, 22, -5)
-  scene.add(cloud)
+  bubbles = new THREE.Sprite(bubblesMaterial)
+  bubbles.scale.set(7, 5, 3)
+  bubbles.position.x = -10
+  bubbles.position.y = 5
+  bubbles.position.z = 10
+  scene.add(bubbles)
+}
+function createFish1() {
+  const geometry = new THREE.PlaneGeometry(10, 10, 1)
+  const texture = new THREE.TextureLoader().load("img/Fish1.png")
+  texture.wrapS = texture.wrapT = THREE.RepeatWrapping
+  const colorMaterial = new THREE.MeshStandardMaterial({
+    map: texture,
+    color: 0xffffff,
+    side: THREE.DoubleSide,
+    transparent: true,
+  })
+
+  fish1 = new THREE.Mesh(geometry, colorMaterial)
+
+  fish1.position.set(-12, -12, 13)
+  scene.add(fish1)
+}
+function createFish2() {
+  const geometry = new THREE.PlaneGeometry(10, 10, 1)
+  const texture = new THREE.TextureLoader().load("img/Fish2.png")
+  texture.wrapS = texture.wrapT = THREE.RepeatWrapping
+  const colorMaterial = new THREE.MeshStandardMaterial({
+    map: texture,
+    color: 0xffffff,
+    side: THREE.DoubleSide,
+    transparent: true,
+  })
+
+  fish2 = new THREE.Mesh(geometry, colorMaterial)
+
+  fish2.position.set(8, -10, 7)
+}
+function createStarFish() {
+  const geometry = new THREE.PlaneGeometry(10, 10, 1)
+  const texture = new THREE.TextureLoader().load("img/starfish.png")
+  texture.wrapS = texture.wrapT = THREE.RepeatWrapping
+  const colorMaterial = new THREE.MeshStandardMaterial({
+    map: texture,
+    color: 0xffffff,
+    side: THREE.DoubleSide,
+    transparent: true,
+  })
+
+  starFish = new THREE.Mesh(geometry, colorMaterial)
+
+  starFish.rotation.x = Math.PI / 2
+  // starFish.rotation.y = (-Math.PI / 4) * 1.15
+  starFish.position.x = 30
+  starFish.position.y = -14
+  starFish.position.z = 5
+  scene.add(starFish)
+  const tween = new TWEEN.Tween(starFish.rotation)
+    .to({ y: Math.PI / 2 }, 10000) // relative animation
+    .onComplete(function () {
+      if (Math.abs(starFish.rotation.x) >= 2 * Math.PI) {
+        starFish.rotation.x = starFish.rotation.x % (2 * Math.PI)
+      }
+    })
+    .start()
+  tween.repeat(Infinity)
+}
+function createGroupFishes() {
+  fishes = new THREE.Group()
+  fishes.add(fish1)
+  fishes.add(fish2)
+  scene.add(fishes)
+
+  const tween = new TWEEN.Tween(fishes.rotation)
+    .to({ y: Math.PI / 2 }, 10000) // relative animation
+    .onComplete(function () {
+      if (Math.abs(fishes.rotation.y) >= 2 * Math.PI) {
+        fishes.rotation.y = fishes.rotation.y % (2 * Math.PI)
+      }
+    })
+    .start()
+  tween.repeat(Infinity)
 }
 
 // Meshes and other visible objects
 function createMeshes() {
-  createPlane()
-  createSideWall()
-  const sideWall2 = createSideWall()
-  sideWall2.position.z = -3
-  createBackWall()
-  const roof1 = createRoof()
-  const roof2 = createRoof()
-  roof1.position.x = -5.5
-  roof2.rotation.y = (Math.PI / 4) * 1.15
-  roof2.position.x = -14
-  createFrontWall()
-  createRod()
-  const rod2 = createRod()
-  rod2.position.x = 5
-  const rod3 = createRod()
-  rod3.position.z = 9
-  const rod4 = createRod()
-  rod4.position.z = 9
-  rod4.position.x = 7
-  createGarageRoof()
-  createDoor()
-  createWindow()
-  createFloor()
-  createWater()
-  createCloud()
+  createBubbles()
+  createFish1()
+  createFish2()
+  createStarFish()
+  createGroupFishes()
 }
 
 // Renderer object and features
